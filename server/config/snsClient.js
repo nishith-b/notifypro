89de97e4149e76f8349c7ec221bac331702b3a23
nishith <nishith.one@gmail.com>
2025-09-10 19:58:01 +0530

    feat(aws): setup SNS topic and client configuration

---
+++ server/config/snsClient.js
@@ -0,0 +1,13 @@
+// snsClient.js
+const { SNSClient } = require("@aws-sdk/client-sns");
+
+// Create SNS client
+const snsClient = new SNSClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+module.exports = { snsClient };
